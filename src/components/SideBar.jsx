7d9f430be6881b1f72748106580fce936c2b1a65
Muzamil-Khaan <muzamilpukhtun@gmail.com>
2023-12-30 11:10:15 -0800

    Initial commit

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,33 @@
+import React from '@mui/material';
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { categories } from '../utils/constants';
+
+const SideBar = ({selectedCategory,setSelectedCategory}) => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflow: 'auto',
+        height: { sx: 'auto', md: '95%' },
+        flexDirection: { md: 'column' },
+      }}
+    >
+      {categories.map((category) => (
+        <button key={category.name}
+        onClick={()=>setSelectedCategory(category.name)}
+        className="category-btn"
+        style={{
+            background:category.name===selectedCategory && '#FC1503',
+            color:'white'
+        }}
+        >
+          <span style={{color:category.name===selectedCategory?'white':'red',marginRight:'15px'}}>{category.icon}</span>
+          <span style={{opactiy:category.name===selectedCategory?'1':'0.8'}}>{category.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+}
+
+export default SideBar;
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,93 @@
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { useState, useEffect } from "react";
+
+import { Video, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [videos, setVideos] = useState(null);
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => {
+      console.log(data); // Log the response to check its structure
+      setVideoDetail(data.items[0]);
+      setLoading(false);
+
+      fetchFromAPI(
+        `search?part=snippet&relatedToVideoId=${id}&type=video`
+      ).then((data) => setVideos(data.items));
+    });
+  }, [id]);
+  if (!videoDetail) {
+    // Handle the case where videoDetail is null (e.g., show loading indicator)
+    return <div>Loading...</div>;
+  }
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh" width="100vw">
+      {videoDetail && (
+        <Stack direction={{ xs: "column", md: "row" }}>
+          <Box flex={1}>
+            <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${id}`}
+                className="react-player"
+                controls
+              />
+              <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+                {title}
+              </Typography>
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                sx={{ color: "#fff" }}
+                py={1}
+                px={2}
+              >
+                <Link to={`/channel/${channelId}`}>
+                  <Typography
+                    variant={{ sm: "subtitle1", md: "h6" }}
+                    color="#fff"
+                  >
+                    {channelTitle}
+                    <CheckCircle
+                      sx={{ fontSize: "12px", color: "grey", ml: "5px" }}
+                    />
+                  </Typography>
+                </Link>
+                <Stack direction="row" gap="20px" alignItems="center">
+                  <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                    {parseInt(viewCount).toLocaleString()} views
+                  </Typography>
+                  <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                    {parseInt(likeCount).toLocaleString()} likes
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Box>
+          </Box>
+          <Box
+            px={2}
+            py={{ md: 1, xs: 5 }}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Videos videos={videos} direction="column" />
+          </Box>
+        </Stack>
+      )}
+    </Box>
+  );
+};
+
+export default VideoDetail;
